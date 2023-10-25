5d29728bd563832cb9f2d77e8963d706ca522baa
Savana <rodriguezsavana543@gmail.com>
2023-10-25 15:46:02 -0700

    creating an account number generator

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ class bankAccount:
         self.account_number = self.generate_account_number()
         self.balance = 0
 
+# Generate Account Number
+def generate_account_number(self):
+    return str(random.randint(10000000, 99999999))
