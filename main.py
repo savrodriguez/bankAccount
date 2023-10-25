85534ad9999e4d794be5687d523020bb83a25b94
Savana <rodriguezsavana543@gmail.com>
2023-10-25 15:51:12 -0700

    deposit and withdraw functions

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ class bankAccount:
 # Generate Account Number
 def generate_account_number(self):
     return str(random.randint(10000000, 99999999))
+
+
+# Deposits 
+def deposit(self, amount):
+    self.balance += amount
+    print(f"Amount Depositited: ${amount:.2f}")
+    print(f"New Balance: ${self.balance:.2f}")
+
+#Withdrawl
+def withdrawl(self, amount):
+    if amount <= self.balance:
+        self.balance -= amount
+        print(f"Amount Withdrawn: ${amount:.2f}")
+        print(f"New Balance: ${self.balance:.2f}")
+
+    else: 
+        print("Insufficient Funds")
+        self.balance -= 10
+        print(f"Overdraft fee of $10 has been charged!")
+
