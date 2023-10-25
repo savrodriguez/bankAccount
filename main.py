18079ca6926ce9fa949a5c2a4bb243d11fdb5413
Savana <rodriguezsavana543@gmail.com>
2023-10-25 16:01:52 -0700

    testing and minor tweak

--- main.py
+++ main.py
@@ -6,39 +6,55 @@ class bankAccount:
         self.account_number = self.generate_account_number()
         self.balance = 0
 
-# Generate Account Number
-def generate_account_number(self):
-    return str(random.randint(10000000, 99999999))
+    # Generate Account Number
+    def generate_account_number(self):
+        return str(random.randint(10000000, 99999999))
 
 
-# Deposits 
-def deposit(self, amount):
-    self.balance += amount
-    print(f"Amount Depositited: ${amount:.2f}")
-    print(f"New Balance: ${self.balance:.2f}")
-
-#Withdrawl
-def withdrawl(self, amount):
-    if amount <= self.balance:
-        self.balance -= amount
-        print(f"Amount Withdrawn: ${amount:.2f}")
+    # Deposits 
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount Depositited: ${amount:.2f}")
         print(f"New Balance: ${self.balance:.2f}")
 
-    else: 
-        print("Insufficient Funds")
-        self.balance -= 10
-        print(f"Overdraft fee of $10 has been charged!")
+    #Withdrawl
+    def withdrawl(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Amount Withdrawn: ${amount:.2f}")
+            print(f"New Balance: ${self.balance:.2f}")
 
-# Getting Balance
-def getBalance(self):
-    print(f"Balance: ${self.balance:.2f}")
-    return self.balance
+        else: 
+            print("Insufficient Funds")
+            self.balance -= 10
+            print(f"Overdraft fee of $10 has been charged!")
 
-# Print Statement
-def printStatement(self):
-    print(f"{self.name}\nAccount No.: ****{self.account_number[-4:]}\nBalance: ${self.balance:.2f}")
+    # Getting Balance
+    def getBalance(self):
+        print(f"Balance: ${self.balance:.2f}")
+        return self.balance
 
-# Add Interest 
-def interest(self):
-    interest = self.balance * 0.00083
-    self.balance += interest
+    # Print Statement
+    def printStatement(self):
+        print(f"{self.name}\nAccount No.: ****{self.account_number[-4:]}\nBalance: ${self.balance:.2f}")
+
+    # Add Interest 
+    def interest(self):
+        interest = self.balance * 0.00083
+        self.balance += interest
+
+
+# TEST
+
+savAccount = bankAccount("Savana")
+
+savAccount.deposit(1500)
+savAccount.printStatement()
+
+savAccount.interest()
+savAccount.printStatement()
+
+savAccount.withdrawl(500)
+savAccount.printStatement()
+
+
